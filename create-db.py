6cef3ac6c9bab524871b6f038e9d0fa6232a21c0
Fineen Davis <fdavi093@uottawa.ca>
2020-11-27 13:25:59 -0500

    eddie's script

--- create-db.py
+++ create-db.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
-# Command: sqlite3 -csv michif.sqlite3 '.header on' 'SELECT * FROM verb' > michif.csv
-
-
 import json
 import sqlite3
+import os
 
-with open("michif.json", encoding="UTF-8") as f:
+with open("/Volumes/Data/github_repos/gramble_ts/Week_3/MichifVerb_Sheet1.json", encoding="UTF-8") as f:
     json_db = json.load(f)
 
-conn = sqlite3.connect("michif.sqlite3")
+conn = sqlite3.connect("MichifVerb.sqlite3")
 
 """
     "person": "1",
@@ -21,6 +19,9 @@ conn = sqlite3.connect("michif.sqlite3")
     "subject": "1SG",
     "object": "3SG",
     "text": "gii-ayaan",
+    "preverb: "nohtee",
+    "verb": "ayaan",
+    "verb_translation"
     "gloss": "1-PST-null-have-VAIT-IND-1SG-",
     "p": "2.03e-8"
 """
@@ -31,11 +32,14 @@ conn.executescript(
 
     CREATE TABLE verb (
         text    TEXT NOT NULL,
+        preverb TEXT,
+        verb    TEXT NOT NULL,
+        verb_translation TEXT NOT NULL,
         type    TEXT NOT NULL,
         person  TEXT,
         tense   TEXT,
         mood    TEXT NOT NULL,
-        `order` TEXT NOT NULL,
+        `order` TEXT,
         subject TEXT NOT NULL,
         object TEXT,
         gloss   TEXT NOT NULL
@@ -46,7 +50,7 @@ conn.executescript(
 
 def generate_rows():
     for entry in json_db:
-        for key in "person", "tense", "type", "mood", "order", "subject", "object", "gloss":
+        for key in "person", "preverb", "verb", "verb_translation", "type", "tense", "mood", "order", "subject", "object", "gloss":
             entry.setdefault(key, None)
         yield entry
 
@@ -54,8 +58,10 @@ def generate_rows():
 with conn:
     conn.executemany(
         """
-        INSERT INTO verb (text, person, tense, type, mood, `order`, subject, object, gloss)
-        VALUES (:text, :person, :tense, :type, :mood, :order, :subject, :object, :gloss);
+        INSERT INTO verb (text, preverb, verb, verb_translation, type, person, tense, mood, `order`, subject, object, gloss)
+        VALUES (:text, :preverb, :verb, :verb_translation, :type, :person, :tense, :mood, :order, :subject, :object, :gloss);
         """,
         generate_rows(),
     )
+
+os.system("sqlite3 -csv MichifVerb.sqlite3 '.header on' 'SELECT * FROM verb' > MichifVerb.csv")
